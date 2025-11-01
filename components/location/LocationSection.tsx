d6ac63142f55d9495abed085981fcfd1248e8fca
felix <ffelixrichardo@gmail.com>
2025-11-02 01:14:44 +0700

    compress image

--- components/location/LocationSection.tsx
+++ components/location/LocationSection.tsx
@@ -10,7 +10,7 @@ export default function LocationSection() {
       <FrameImage
         backgroundImgUrl="/location/BGFrameImage.svg"
         titleImgUrl="/location/LokasiKumpul.svg"
-        mainImgUrl="/location/corepreneur.png"
+        mainImgUrl="/location/corepreneur.webp"
         bottomElement={
           <>
             <p className="text-md sm:text-2xl md:text-3xl"> Open Gate: 06.45</p>
@@ -21,7 +21,7 @@ export default function LocationSection() {
       <FrameImage
         backgroundImgUrl="/location/BGFrameImage.svg"
         titleImgUrl="/location/LokasiCampJurusan.svg"
-        mainImgUrl="/location/ayannahotel.png"
+        mainImgUrl="/location/ayannahotel.webp"
         bottomElement={
           <>
             <Link href="https://maps.app.goo.gl/RCUCRXCw32gVeWmu9" className="text-md sm:text-2xl md:text-3xl"> Maps to Arayanna</Link>
